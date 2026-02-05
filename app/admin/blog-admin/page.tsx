a883ad21cb22c2000ba17383f4724161b8890977
artificialxflow <artificialxflow@gmail.com>
2026-02-05 05:51:59 -0600

    next

--- app/admin/blog-admin/page.tsx
+++ app/admin/blog-admin/page.tsx
@@ -3,5 +3,5 @@ import BlogAdminPage from '@/app/blog-admin/page';
 export const dynamic = 'force-dynamic';
 
 export default function AdminBlogAdminPage() {
-  return <BlogAdminPage noLayout />;
+  return <BlogAdminPage />;
 }
